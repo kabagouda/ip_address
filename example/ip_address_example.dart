4249e17b95043e02a325296e3e61d07b70273eb9
kagouda <technodor7@gmail.com>
2022-02-15 15:16:07 +0000

    remove unused import

--- example/ip_address_example.dart
+++ example/ip_address_example.dart
@@ -1,9 +1,7 @@
-import 'dart:typed_data';
-
 import 'package:ip_address/ip_address.dart';
 
 void main() async {
-  var ip = await IpAddress.getOffsetFor('208.67.222.222');
-   print(ip);
+  String ip = await IpAddress.getIp();
+  print(ip);
   //Output : 208.67.222.222
 }
--- test/ip_address_test.dart
+++ test/ip_address_test.dart
@@ -1,6 +1,4 @@
-import 'package:ip_address/ip_address.dart';
 import 'package:test/test.dart';
-
 void main() {
   group('A group of tests', () {
     // final awesome = Awesome();
